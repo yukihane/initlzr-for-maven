edff0c49d97b3a0658f77fcc899c4ae5a8c1e208
yukihane <yukihane.feather@gmail.com>
2022-06-01 03:28:12 +0900

    ボタンを押してrust関数を呼ぶ

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { invoke } from "@tauri-apps/api";
+import React from "react";
 
-function App() {
+export const App = () => {
+  const handleClicked = () => {
+    console.log("click");
+    invoke("generate")
+      .then((res) => console.log("OK: " + res))
+      .catch((e) => console.log("NG: " + e));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => handleClicked()}>生成</button>
     </div>
   );
-}
-
-export default App;
+};
