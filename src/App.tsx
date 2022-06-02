b837bd37406ce6615b622741c643759557989670
yukihane <yukihane.feather@gmail.com>
2022-06-03 02:24:21 +0900

    コンテンツを保存する

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { invoke } from "@tauri-apps/api";
+import { fs, invoke } from "@tauri-apps/api";
+import { save } from "@tauri-apps/api/dialog";
 import React from "react";
 
 export const App = () => {
+  const saveContent = (contents: string) => {
+    save().then((path) => {
+      if (path === null) {
+        return;
+      }
+      fs.writeFile({
+        contents,
+        path,
+      });
+    });
+  };
+
   const handleClicked = () => {
     console.log("click");
-    invoke("generate")
-      .then((res) => console.log("OK: " + res))
+    invoke<string>("generate")
+      .then((res) => saveContent(res))
       .catch((e) => console.log("NG: " + e));
   };
+
   return (
     <div>
       <button onClick={() => handleClicked()}>生成</button>
